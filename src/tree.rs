5e12278643a7bdeb1f6a1b19b654068faa889cdb
Tom Webber <t.c.w.webber@gmail.com>
2024-12-07 21:01:49 +0100

    (tree) fix: missing types

--- src/tree.rs
+++ src/tree.rs
@@ -2,49 +2,64 @@ trait Operator {
     fn precedence(&self) -> u32;
 }
 
+enum Associativity {
+    LeftToRight,
+    RightToLeft,
+}
+
 struct Binary {
     operator: BinaryOperator,
     arg_l: Box<Node>,
     arg_r: Box<Node>,
 }
 enum BinaryOperator {
-    Times,
+    // `[]`
+    ArraySubscript,
+    // (`.`)
+    StructEnumMemberAccess,
+    // (`->`)
+    StructEnumMemberPointerAccess,
+    Multiply,
     Divide,
     Modulo,
-    /*  */
-    Plus,
-    Minus,
-    /*  */
+    Add,
+    Subtract,
     BitwiseRightShift,
     BitwiseLeftShift,
-    /*  */
     LessThan,
     LessEqual,
     GreaterThan,
     GreaterEqual,
-    /*  */
     Equal,
     Different,
-    /* */
     BitwiseAnd,
-    /* */
     BitwiseXor,
-    /* */
     BitwiseOr,
-    /* */
     LogicalAnd,
-    /* */
     LogicalOr,
-    /* */
-    // assignment operators
+    Assign,
+    AddAssign,
+    SubtractAssign,
+    MultiplyAssign,
+    DivivdeAssign,
+    ModuloAssign,
+    BitwiseLeftShiftAssign,
+    BitwiseRightShiftAssign,
+    BitwiseAndAssign,
+    BitwiseXorAssign,
+    BitwiseOrAssign,
+    Comma,
 }
 
 impl Operator for BinaryOperator {
     fn precedence(&self) -> u32 {
         use BinaryOperator as BO;
         match self {
-            BO::Times | BO::Divide | BO::Modulo => 3,
-            BO::Plus | BO::Minus => 4,
+            BO::ArraySubscript | BO::StructEnumMemberAccess | BO::StructEnumMemberPointerAccess => {
+                1
+            }
+            BO::Multiply | BO::Divide | BO::Modulo => 3,
+            BO::Add | BO::Subtract => 4,
             BO::BitwiseRightShift | BO::BitwiseLeftShift => 5,
             BO::LessThan | BO::LessEqual | BO::GreaterThan | BO::GreaterEqual => 6,
             BO::Equal | BO::Different => 7,
@@ -53,20 +68,65 @@ impl Operator for BinaryOperator {
             BO::BitwiseOr => 10,
             BO::LogicalAnd => 11,
             BO::LogicalOr => 12,
+            BO::Assign
+            | BO::AddAssign
+            | BO::SubtractAssign
+            | BO::MultiplyAssign
+            | BO::DivivdeAssign
+            | BO::ModuloAssign
+            | BO::BitwiseLeftShiftAssign
+            | BO::BitwiseRightShiftAssign
+            | BO::BitwiseAndAssign
+            | BO::BitwiseXorAssign
+            | BO::BitwiseOrAssign => 14,
+            BO::Comma => 15,
         }
     }
 }
 
-pub enum Node {
+struct CompoundLiteral {
+    operator: CompoundLiteralOperator,
+    type_: String,
+    args: Vec<Box<Node>>,
+}
+
+struct CompoundLiteralOperator;
+
+impl Operator for CompoundLiteralOperator {
+    fn precedence(&self) -> u32 {
+        1
+    }
+}
+struct Function {
+    operator: FunctionOperator,
+    name: String,
+    args: Vec<Box<Node>>,
+}
+
+struct FunctionOperator;
+
+impl Operator for FunctionOperator {
+    fn precedence(&self) -> u32 {
+        1
+    }
+}
+
+enum Node {
     Leaf(Literal),
     Unary(Unary),
     Binary(Binary),
     Ternary(Ternary),
+    Function(Function),
+    CompoundLiteral(CompoundLiteral),
+    Vec(Vec<Box<Node>>),
 }
 
 enum Literal {
+    /// # Constants
+    /// All constants (int, float, char, string, ...)
+    /// For exemple, a string will be stored as `"\"Hellow\""`.
+    Const(String),
     String(String),
-    Number(u32),
     Variable(String),
 }
 
@@ -92,39 +152,40 @@ struct Unary {
 
 enum UnaryOperator {
     Defined,
-    /*  */
     PostfixIncrement,
     PostfixDecrement,
-    Function(String),
-    // []
-    // ., ->
-    // compound literal
-    /*  */
     PrefixIncrement,
     PrefixDecrement,
     Plus,
     Minus,
     BitwiseNot,
     LogicalNot,
-    // compound literal
-    // *, &
-    // sizeof, _Alignof
-    /*  */
+    Cast(String),
+    /// Dereference (`*`)
+    Indirection,
+    /// Address-of (`&`)
+    AddressOf,
+    SizeOf,
+    AlignOf,
 }
 
 impl Operator for UnaryOperator {
     fn precedence(&self) -> u32 {
+        use UnaryOperator as UO;
         match self {
-            UnaryOperator::Defined => 0,
-            UnaryOperator::PostfixIncrement
-            | UnaryOperator::PostfixDecrement
-            | UnaryOperator::Function(_) => 1,
-            UnaryOperator::PrefixIncrement
-            | UnaryOperator::PrefixDecrement
-            | UnaryOperator::Plus
-            | UnaryOperator::Minus
-            | UnaryOperator::BitwiseNot
-            | UnaryOperator::LogicalNot => 2,
+            UO::Defined => 0,
+            UO::PostfixIncrement | UO::PostfixDecrement => 1,
+            UO::PrefixIncrement
+            | UO::PrefixDecrement
+            | UO::Plus
+            | UO::Minus
+            | UO::BitwiseNot
+            | UO::LogicalNot
+            | UO::Cast(_)
+            | UO::Indirection
+            | UO::AddressOf
+            | UO::SizeOf
+            | UO::AlignOf => 2,
         }
     }
 }
