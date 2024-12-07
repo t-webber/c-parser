14f0691ba223ef34cc6b2430b1ca0c25997a49fa
Tom Webber <t.c.w.webber@gmail.com>
2024-12-07 19:18:26 +0100

    (tree) add: precedence

--- src/tree.rs
+++ src/tree.rs
@@ -1,3 +1,7 @@
+trait Operator {
+    fn precedence(&self) -> u32;
+}
+
 struct Binary {
     operator: BinaryOperator,
     arg_l: Box<Node>,
@@ -7,21 +11,50 @@ enum BinaryOperator {
     Times,
     Divide,
     Modulo,
+    /*  */
     Plus,
     Minus,
+    /*  */
     BitwiseRightShift,
     BitwiseLeftShift,
+    /*  */
     LessThan,
     LessEqual,
     GreaterThan,
     GreaterEqual,
+    /*  */
     Equal,
     Different,
+    /* */
     BitwiseAnd,
+    /* */
     BitwiseXor,
+    /* */
     BitwiseOr,
+    /* */
     LogicalAnd,
+    /* */
     LogicalOr,
+    /* */
+    // assignment operators
+}
+
+impl Operator for BinaryOperator {
+    fn precedence(&self) -> u32 {
+        use BinaryOperator as BO;
+        match self {
+            BO::Times | BO::Divide | BO::Modulo => 3,
+            BO::Plus | BO::Minus => 4,
+            BO::BitwiseRightShift | BO::BitwiseLeftShift => 5,
+            BO::LessThan | BO::LessEqual | BO::GreaterThan | BO::GreaterEqual => 6,
+            BO::Equal | BO::Different => 7,
+            BO::BitwiseAnd => 8,
+            BO::BitwiseXor => 9,
+            BO::BitwiseOr => 10,
+            BO::LogicalAnd => 11,
+            BO::LogicalOr => 12,
+        }
+    }
 }
 
 pub enum Node {
@@ -38,9 +71,18 @@ enum Literal {
 }
 
 struct Ternary {
+    operator: TernaryOperator,
     condition: Box<Node>,
     success: Box<Node>,
     failure: Box<Node>,
+}
+
+struct TernaryOperator;
+
+impl Operator for TernaryOperator {
+    fn precedence(&self) -> u32 {
+        13
+    }
 }
 
 struct Unary {
@@ -50,13 +92,39 @@ struct Unary {
 
 enum UnaryOperator {
     Defined,
+    /*  */
     PostfixIncrement,
     PostfixDecrement,
     Function(String),
+    // []
+    // ., ->
+    // compound literal
+    /*  */
     PrefixIncrement,
     PrefixDecrement,
     Plus,
     Minus,
     BitwiseNot,
     LogicalNot,
+    // compound literal
+    // *, &
+    // sizeof, _Alignof
+    /*  */
+}
+
+impl Operator for UnaryOperator {
+    fn precedence(&self) -> u32 {
+        match self {
+            UnaryOperator::Defined => 0,
+            UnaryOperator::PostfixIncrement
+            | UnaryOperator::PostfixDecrement
+            | UnaryOperator::Function(_) => 1,
+            UnaryOperator::PrefixIncrement
+            | UnaryOperator::PrefixDecrement
+            | UnaryOperator::Plus
+            | UnaryOperator::Minus
+            | UnaryOperator::BitwiseNot
+            | UnaryOperator::LogicalNot => 2,
+        }
+    }
 }
