9780dca100bb5287b5da6ac721e0502ea56e3792
Tom Webber <t.c.w.webber@gmail.com>
2024-12-14 20:55:29 +0100

    (tree) add: get and set last leaf in AST

--- src/parser/tree.rs
+++ src/parser/tree.rs
@@ -1,4 +1,5 @@
 use core::fmt;
+use core::mem;
 
 use crate::lexer::api::types::Number;
 
@@ -7,13 +8,45 @@ pub trait Operator: fmt::Debug {
     fn associativity(&self) -> Associativity;
 }
 
-#[derive(Debug)]
+trait AddArgument: Into<Node> {
+    fn add_argument(&mut self, arg: Node) -> bool;
+}
+
+trait FromOperator<T: AddArgument> {
+    fn from_operator(self) -> T;
+}
+
+impl Into<Node> for Unary {
+    fn into(self) -> Node {
+        Node::Unary(self)
+    }
+}
+
+impl FromOperator<Unary> for UnaryOperator {
+    fn from_operator(self) -> Unary {
+        Unary {
+            operator: self,
+            arg: None,
+        }
+    }
+}
+impl FromOperator<Binary> for BinaryOperator {
+    fn from_operator(self) -> Binary {
+        Binary {
+            operator: self,
+            arg_l: None,
+            arg_r: None,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Associativity {
     LeftToRight,
     RightToLeft,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Binary {
     operator: BinaryOperator,
     arg_l: Option<Box<Node>>,
@@ -30,7 +63,32 @@ impl From<BinaryOperator> for Binary {
     }
 }
 
-#[derive(Debug)]
+impl Into<Node> for Binary {
+    fn into(self) -> Node {
+        Node::Binary(self)
+    }
+}
+
+impl AddArgument for Binary {
+    fn add_argument(&mut self, arg: Node) -> bool {
+        if let Binary {
+            arg_l: op_arg @ None,
+            ..
+        }
+        | Binary {
+            arg_r: op_arg @ None,
+            ..
+        } = self
+        {
+            *op_arg = Some(Box::from(arg));
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub enum BinaryOperator {
     // `[]`
     ArraySubscript,
@@ -140,14 +198,14 @@ impl Operator for BinaryOperator {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct CompoundLiteral {
     args: Vec<Node>,
     operator: CompoundLiteralOperator,
     type_: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct CompoundLiteralOperator;
 
 impl Operator for CompoundLiteralOperator {
@@ -160,14 +218,14 @@ impl Operator for CompoundLiteralOperator {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct FunctionCall {
     name: String,
     operator: FunctionOperator,
     args: Vec<Node>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct FunctionOperator;
 
 impl Operator for FunctionOperator {
@@ -180,8 +238,10 @@ impl Operator for FunctionOperator {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Default)]
 pub enum Literal {
+    #[default]
+    Empty,
     String(String),
     Variable(String),
     Char(char),
@@ -189,7 +249,7 @@ pub enum Literal {
     Number(Number),
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub enum Node {
     #[default]
     Empty,
@@ -204,10 +264,11 @@ pub enum Node {
 }
 
 impl Node {
-    pub fn try_push_leaf(&mut self, literal: Literal) -> Result<(), &'static str> {
-        let node_leaf = Self::Leaf(literal);
+    /// This functions returns Err if two many arguments were provided,
+    /// like in the expression: `a+b c`.
+    pub fn push_node_as_leaf(&mut self, node: Node) -> Result<(), &'static str> {
         match self {
-            Self::Empty => *self = node_leaf,
+            Self::Empty => *self = node,
             // push in Option<Box<Node>>
             Self::Binary(
                 Binary {
@@ -235,13 +296,11 @@ impl Node {
             )
             | Self::Unary(Unary {
                 arg: last @ None, ..
-            }) => *last = Some(Box::new(node_leaf)),
+            }) => *last = Some(Box::new(node)),
             // push in Vec<Node>
-            Self::Block(vec)
-            | Self::Vec(vec)
-            | Self::FunctionCall(FunctionCall { args: vec, .. }) => vec.push(node_leaf),
+            Self::Block(vec) => vec.push(node),
             // todo
-            Self::CompoundLiteral(_) => todo!(),
+            Self::Vec(_) | Self::FunctionCall(_) | Self::CompoundLiteral(_) => todo!(),
             // Errors
             Self::Leaf(_) => {
                 return Err("Found 2 consecutive litteral without a logical relation.")
@@ -260,21 +319,103 @@ impl Node {
         };
         Ok(())
     }
-}
 
-impl From<UnaryOperator> for Node {
-    fn from(value: UnaryOperator) -> Self {
-        Node::Unary(Unary::from(value))
+    pub fn take_last_leaf(&mut self) -> Option<Literal> {
+        match self {
+            node @ Self::Leaf(_) => {
+                if let Self::Leaf(leaf) = mem::replace(node, Node::Empty) {
+                    Some(leaf)
+                } else {
+                    panic!("never happens because old is leaf...")
+                }
+            }
+            Self::Binary(
+                Binary {
+                    arg_r: Some(child), ..
+                }
+                | Binary {
+                    arg_l: Some(child), ..
+                },
+            )
+            | Self::Ternary(
+                Ternary {
+                    failure: Some(child),
+                    ..
+                }
+                | Ternary {
+                    success: Some(child),
+                    ..
+                }
+                | Ternary {
+                    condition: Some(child),
+                    ..
+                },
+            )
+            | Self::Unary(Unary {
+                arg: Some(child), ..
+            }) => child.take_last_leaf(),
+            Self::Block(vec) => {
+                if let Some(last) = vec.last_mut() {
+                    last.take_last_leaf()
+                } else {
+                    None
+                }
+            }
+            // todo
+            Self::Vec(_) | Self::FunctionCall(_) | Self::CompoundLiteral(_) => todo!(),
+            // Errors
+            _ => None,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        *self == Node::Empty
+    }
+
+    pub fn push_op<U: AddArgument, T: Operator + FromOperator<U> + Into<Node>>(
+        &mut self,
+        operator: T,
+    ) -> Result<(), &'static str> {
+        //TODO: this doesn't work for cast, sizeof and alignof
+        match operator.associativity() {
+            Associativity::LeftToRight => match self.take_last_leaf() {
+                None => {
+                    // This is error is never printed, because the only left-to-right operators are postfix increments, and those are catched.
+                    return Err(
+                        "Found left-to-right unary operator, without having a leaf before.",
+                    );
+                }
+                Some(leaf) => {
+                    let mut new_leaf = operator.from_operator();
+                    new_leaf.add_argument(Node::Leaf(leaf));
+                    self.push_node_as_leaf(new_leaf.into());
+                }
+            },
+            Associativity::RightToLeft => {
+                if let Err(_) = self.push_node_as_leaf(operator.into()) {
+                    // Example: `int c = a+b!;`
+                    return Err(
+                        "Found right-to-left unary operator, within a context not waiting for leaf.",
+                    );
+                }
+            }
+        }
+        Ok(())
     }
 }
 
-impl From<BinaryOperator> for Node {
-    fn from(value: BinaryOperator) -> Self {
-        Node::Binary(Binary::from(value))
+impl Into<Node> for UnaryOperator {
+    fn into(self) -> Node {
+        Node::Unary(Unary::from(self))
+    }
+}
+impl Into<Node> for BinaryOperator {
+    fn into(self) -> Node {
+        Node::Binary(Binary::from(self))
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Ternary {
     operator: TernaryOperator,
     condition: Option<Box<Node>>,
@@ -282,7 +423,7 @@ pub struct Ternary {
     failure: Option<Box<Node>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct TernaryOperator;
 
 impl Operator for TernaryOperator {
@@ -295,7 +436,7 @@ impl Operator for TernaryOperator {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Unary {
     operator: UnaryOperator,
     arg: Option<Box<Node>>,
@@ -310,9 +451,24 @@ impl From<UnaryOperator> for Unary {
     }
 }
 
-#[derive(Debug)]
+impl AddArgument for Unary {
+    fn add_argument(&mut self, arg: Node) -> bool {
+        if let Unary {
+            arg: old_arg @ None,
+            ..
+        } = self
+        {
+            *old_arg = Some(Box::new(arg));
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub enum UnaryOperator {
-    Defined,
+    // Defined,
     PostfixIncrement,
     PostfixDecrement,
     PrefixIncrement,
@@ -333,9 +489,7 @@ pub enum UnaryOperator {
 impl Operator for UnaryOperator {
     fn associativity(&self) -> Associativity {
         match self {
-            Self::Defined | Self::PostfixIncrement | Self::PostfixDecrement => {
-                Associativity::LeftToRight
-            }
+            Self::PostfixIncrement | Self::PostfixDecrement => Associativity::LeftToRight,
             Self::PrefixIncrement
             | Self::PrefixDecrement
             | Self::Plus
@@ -352,7 +506,6 @@ impl Operator for UnaryOperator {
 
     fn precedence(&self) -> u32 {
         match self {
-            Self::Defined => 0,
             Self::PostfixIncrement | Self::PostfixDecrement => 1,
             Self::PrefixIncrement
             | Self::PrefixDecrement
